a75a79f27a9ebe084bf7c8154b7f36402496856e
Cesario Nivar <ing.cesarionivar@gmail.com>
2024-01-21 00:01:20 -0500

    Fecth Characters and load it

---
+++ DragonBall/Models/DragonBallModel.swift
@@ -0,0 +1,8 @@
+//
+//  DragonBallModel.swift
+//  DragonBall
+//
+//  Created by Cesario Nivar on 1/20/24.
+//
+
+import Foundation
---
+++ DragonBall/ViewModels/DragonBallViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DragonBallViewModel.swift
+//  DragonBall
+//
+//  Created by Cesario Nivar on 1/20/24.
+//
+
+import Foundation
